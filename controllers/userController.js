9f56c0955805e78d1474eba8f74234975a438cec
Faysal Ahmed <info.faysal.32@gmail.com>
2025-07-26 18:44:10 +0600

    new route to find old user for google and github sign in

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,15 @@
 import { getDB } from "../config/db.js";
 
+export const getOnlyEmail = async (req, res) => {
+  const db = getDB();
+  const users = await db
+    .collection("users")
+    .find({}, { projection: { email: 1, _id: 0 } })
+    .toArray();
+  const emails = users.map((user) => user.email);
+  res.send(emails);
+};
+
 export const createUser = async (req, res) => {
   const db = getDB();
   const user = req.body;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createUser, updateLastLogin } from "../controllers/userController.js";
+import { createUser, getOnlyEmail, updateLastLogin } from "../controllers/userController.js";
 
 const router = express.Router();
 
+router.get("/getOnlyEmail", getOnlyEmail);
 router.post("/", createUser);
 router.patch("/last-login", updateLastLogin);
 
