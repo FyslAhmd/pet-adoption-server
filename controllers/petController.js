8f58dff9b8b1bf70bb5c2753b3ad1001d3b28b36
Faysal Ahmed <info.faysal.32@gmail.com>
2025-08-08 08:52:49 +0600

    create route for rescuer to update and delete their pets

--- controllers/petController.js
+++ controllers/petController.js
@@ -1,6 +1,18 @@
 import { getDB } from "../config/db.js";
 import { ObjectId } from "mongodb";
 
+export const getPetsByUser = async (req, res) => {
+  const db = getDB();
+  const pets = await db
+    .collection("pets")
+    .find({
+      rescuer_email: req.query.rescuer_email,
+    })
+    .sort({ created_at: -1 })
+    .toArray();
+  res.send(pets);
+};
+
 export const createPet = async (req, res) => {
   const db = getDB();
   const {
@@ -58,4 +70,23 @@ export const createPet = async (req, res) => {
     console.error("Error in createPet:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
+};
+
+export const updatePet = async (req, res) => {
+  const db = getDB();
+  const { id } = req.params;
+  const updates = req.body;
+  const result = await db
+    .collection("pets")
+    .updateOne({ _id: new ObjectId(id) }, { $set: updates });
+  res.send(result);
+};
+
+export const deletePet = async (req, res) => {
+  const db = getDB();
+  const { id } = req.params;
+  const deleteResult = await db
+    .collection("pets")
+    .deleteOne({ _id: new ObjectId(id) });
+  res.send(deleteResult);
 };
--- routes/petRoutes.js
+++ routes/petRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { createPet } from "../controllers/petController.js";
+import { createPet, deletePet, getPetsByUser, updatePet } from "../controllers/petController.js";
 
 const router = express.Router();
 
+router.get("/", getPetsByUser);
 router.post("/", createPet);
+router.patch("/:id", updatePet);
+router.delete("/:id", deletePet);
 
 export default router;
